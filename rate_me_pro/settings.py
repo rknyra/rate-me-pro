fda2c9ce0178da9cdd81e3263d028a1e654ea18f
RKNyra <rk.nyra@gmail.com>
2019-11-24 01:24:56 +0300

    create the navbar where users can navigate to different pages of the app

--- rate_me_pro/settings.py
+++ rate_me_pro/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rates',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- rates/views.py
+++ rates/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+#VIEWS
+
+#index
+def index(request):
+      
+    return render(request, 'index.html', locals())

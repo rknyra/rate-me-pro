809e274931dc3010ccbdf4854d68c2bc861b9f73
RKNyra <rk.nyra@gmail.com>
2019-11-24 16:48:24 +0300

    create the form classes/models that will be used to display the profile update and project submition forms

---
+++ rates/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Profile, Project
+from pyuploadcare.dj.forms import ImageField
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields=['prof_pic','bio','contact']
+        
+        
+class SubmitProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields=('title','project_pic','description', 'website')
